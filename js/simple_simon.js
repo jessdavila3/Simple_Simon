1bf5cc2f80ba95e76ac0cd2c01ae3fe7ff0f3add
Jesse Davila <jessedavila3@gmail.com>
2016-11-01 09:22:19 -0500

    css and js files for simon game
    split html file into just html with css and js in their own
    directories.

---
+++ js/simple_simon.js
@@ -0,0 +1,81 @@
+/**
+ * Created by jessedavila on 11/1/16.
+ */
+"use strict";
+
+var tiles = $('.tile');
+
+var game = {
+    userSeq: [],
+    simonSeq: [],
+    level: 1,
+    score: 0,
+    start: false
+};
+
+$("#start").click(function() {
+    game.start = true;
+    randomPad();
+});
+
+tiles.click(function() {
+    if (game.start) {
+        var tile = $(this);
+        var value = parseInt(this.value);
+        game.userSeq.push(value);
+        compare();
+    } else {
+        alert('press start');
+    }
+
+});
+
+tiles.hover(
+    function() {
+        $(this).addClass('lit');
+    },
+    function() {
+        $(this).removeClass('lit');
+    }
+);
+
+function randomPad() {
+    var random = Math.floor(Math.random() * 4) + 1;
+    game.simonSeq.push(random);
+    setInterval(lightUp(random),1000);
+    console.log(game.simonSeq);
+}
+
+function compare() {
+    if (game.userSeq.length == game.simonSeq.length) {
+        if (game.userSeq[game.userSeq.length-1] == game.simonSeq[game.simonSeq.length-1]) {
+            newLevel();
+            console.log('equal');
+        } else if (game.userSeq[game.userSeq.length-1] !== game.simonSeq[game.simonSeq.length-1]) {
+            youLost();
+        }
+    } else if (game.userSeq[game.userSeq.length-1] !== game.simonSeq[game.userSeq.length-1]) {
+        youLost();
+    }
+}
+
+function newLevel() {
+    ++game.level;
+    randomPad();
+    game.userSeq = [];
+}
+
+function youLost() {
+    alert('you lost');
+    game.start = false;
+    game.userSeq = [];
+    game.simonSeq = [];
+}
+
+function lightUp(data) {
+    $("[value="+data+"]").addClass("lit");
+}
+function lightDown(data) {
+    $("[value="+data+"]").removeClass("lit");
+}
+
